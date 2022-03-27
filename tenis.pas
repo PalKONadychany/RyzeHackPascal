ee8fbda6ec9a5646314c31aef9248d430d45c0fa
PalKONadychany <PalKONadychany@github.com>
2022-03-27 17:55:45 +0200

    pridan pocitadlo tenisu

---
+++ tenis.pas
@@ -0,0 +1,77 @@
+program Tenis;
+uses crt;
+
+var
+  a,b,c,x,y,z,t:integer;
+  //do it t:string, mosis dat ty cisla do uvozovek
+  i,j,n,m: string;
+
+begin
+ClrScr;
+writeln('TENIS');
+writeln(' ');
+writeln('Zadej jmeno pro Hrace 1');
+readln(m);
+writeln('Zadej jmeno pro Hrace 2');
+readln(n);
+Writeln('--------------------------------------------');
+writeln('pouziti');
+writeln('pokud hrac ',m,' skoruje, stiskni klavesu 1');
+writeln('pokud hrac ',n,' skoruje, stiskni klavesu 2');
+writeln('pro ukonceni programu stiskni klavesu 0');
+Writeln('--------------------------------------------');
+repeat
+  repeat
+    repeat
+      writeln('Kdo ziskal bod? hrac 1/2?');
+      read(t);
+      case t of
+       1:a:=a+1;
+       2:x:=x+1;
+       0:begin writeln('zapas prerusen, koncim'); exit; end;
+        else writeln('Chyba: Spantna klavesa. Stiskni  1 nebo 2 pro zvyseni skore. Pro ukonceni stiskni 0 ');
+      end;
+      if a=0 then j:='0'; if x=0 then i:='0';
+      if a=1 then j:='15'; if x=1 then i:='15';
+      if a=2 then j:='30'; if x=2 then i:='30';
+      if a=3 then j:='40'; if x=3 then i:='40';
+      if (a=4)and(x=4) then begin  a:=3; x:=3; i:='40';j:='40'; end;
+      if (a=4)and(x=3) then begin j:='AD'; i:=' '; end;
+      if (a=3)and(x=4) then begin i:='AD'; j:=' '; end;
+      Writeln('--------------------------------------------');
+      Writeln('Stav Hry:');
+      writeln(m,'(Hrac 1): sety:',c, ' gamy:',b,' body:',j);
+      writeln(n,'(Hrac 2): sety:',z, ' gamy:',y,' body:',i);
+      Writeln('--------------------------------------------');
+    until (a>3)and(a>x+1) or (x>3)and(x>a+1);
+
+
+    if (a>3)and(a>x+1) then b:=b+1
+    else y:=y+1;
+    a:=0; x:=0; i:='0'; j:='0';
+    Writeln('--------------------------------------------');
+    Writeln('Stav hry:');
+    writeln(m,'(Hrac 1): sety:',c, ' gamy:',b,' body:',j);
+    writeln(n,'(Hrac 2): sety:',z, ' gamy:',y,' body:',i);
+    Writeln('--------------------------------------------');
+
+  until (b>5)and(b>y+1) or (y>5)and(y>b+1);
+  if (b>5)and(b>y+1)
+
+  then c:=c+1
+  else z:=z+1;
+  b:=0;y:=0;
+  Writeln('--------------------------------------------');
+  Writeln('Stav hry:');
+  writeln(m,'(Hrac 1): sety:',c, ' gamy:',b,' body:',j);
+  writeln(n,'(Hrac 2): sety:',z, ' gamy:',y,' body:',i);
+  Writeln('--------------------------------------------');
+
+
+until (c=3) or (z=3);
+
+if c=3 then writeln('Vyhrava ',m)
+else writeln('Vyhrava' ,n);
+
+readln();
+end.
