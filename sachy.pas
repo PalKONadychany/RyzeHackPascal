85e0b993b14e0a4ce9660b942d4637aadd3742d1
PalKONadychany <PalKONadychany@github.com>
2022-05-04 22:47:47 +0200

    sachy

--- sachy.pas
+++ sachy.pas
@@ -1,18 +1,58 @@
 program sachy;
 uses crt;
-var a,i,j:integer;
+var a,b,c,d,e,n,j:integer;
+i:array[1..20]of integer;
 g: char;
 
 begin
-	clrscr;
+clrscr;
+writeln('zadej pocet hracu');
+readln(n);
+clrscr;
+	for j:=1 to n do begin gotoxy(5+c,1); writeln(j); c:=c+6; end; j:=0; c:=0;
 	repeat
-	read(g);
-	case g of
-	'1':writeln('bavalo');
-	'2':writeln('kavalo');
-	'x':writeln('valo');
-	'5':exit;
-	//else writeln('deges');
-end;
-until g='5';
+	j:=j+1; gotoxy(1,4+d); writeln(j); d:=d+4;
+	until j=n; d:=0;
+	repeat
+		repeat
+		gotoxy(2+c,2+d);
+		writeln(' _____ ');
+		gotoxy(2+c,3+d);
+		writeln('|');
+		gotoxy(2+c,4+d);
+		writeln('|');
+		gotoxy(8+c,3+d);
+		writeln('|');
+		gotoxy(8+c,4+d);
+		writeln('|');
+		gotoxy(2+c,5+d);
+		writeln('|');
+		gotoxy(8+c,5+d);
+		writeln('|');
+		gotoxy(2+c,6+d);
+		writeln(' _____ ');
+		a:=a+1;
+		c:=c+6;
+		until a=n;
+	c:=0; a:=0;
+	d:=d+4;
+	b:=b+1;
+	until b=n;
+	b:=0; d:=0; a:=0; c:=0;
+	repeat
+		e:=e+1; a:=0; c:=0;
+		repeat
+		gotoxy(1,(n+1)*6);
+		read(g);
+		case g of
+		'0':begin c:=c+6; gotoxy(6*e+c-2,4+d);  writeln('0:1'); gotoxy(4+(3*d)div(2),4*e+(2*c)div(3)); writeln('1:0'); a:=a+1; b:=b+1; end;
+		'1':begin c:=c+6; gotoxy(6*e+c-2,4+d);  writeln('1:0'); gotoxy(4+(3*d)div(2),4*e+(2*c)div(3)); writeln('0:1'); a:=a+1; b:=b+1; end;
+		'x':begin c:=c+6; gotoxy(6*e+c-2,4+d);  writeln('0,5'); gotoxy(4+(3*d)div(2),4*e+(2*c)div(3)); writeln('0,5'); a:=a+1; b:=b+1; end;
+		'%':exit;
+		end;
+		until a=(n-e);
+	d:=d+4;
+	until b=(n*(n-1)/2);
+writeln(' ');
+writeln(' ');
 end.
