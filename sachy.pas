888f65de3d655e40457f043e8a4d2727ca1d9bd6
PalKONadychany <PalKONadychany@github.com>
2022-05-06 23:32:16 +0200

    visualni upravy

--- sachy.pas
+++ sachy.pas
@@ -3,14 +3,18 @@ uses crt;
 var a,b,c,d,e,n,j,i,q:integer;
 m:array[1..100]of real;
 g: char; y:real;
-x,z: array[1..10]of integer;
+x,z: array[1..20]of integer;
 
 begin
 clrscr;
 repeat
+	writeln('sachova tabulka. Po zadani poctu hracu pouyivejte tyto klavesy:');
+	writeln('1: zapise vyhru, 0: zapise prohru, x:remiza, %:konec');
+	writeln('');
 	writeln('zadej pocet hracu');
 	readln(n);
-	if n<2 then clrscr; writeln('Musi se zucastnit alespon 2 hraci');
+	if n<2 then begin clrscr; writeln('Musi se zucastnit alespon 2 hraci'); end;
+	if n>13 then begin writeln('tabulka je urcena pro pocet hracu mensi nez deset. Od 14 a vys je zdeformovana'); readln(); end;
 until n>1;
 clrscr;
 	for j:=1 to n do begin gotoxy(5+c,1); writeln(j); c:=c+6; end; j:=0; c:=0;
@@ -35,8 +39,10 @@ clrscr;
 	repeat
 		e:=e+1;  a:=0; c:=0;
 		repeat
-			gotoxy(1,40);
-			gotoxy(1,(n+1)*6);
+
+			gotoxy(1,(n+1)*5); writeln('                                                            ');
+			gotoxy(1,(n+1)*5-1); writeln('zapas hrace ',q,' vs hrac ',a+q+1,'  ');
+			gotoxy(1,(n+1)*5);
 			read(g);
 			case g of
 			'0':begin c:=c+6; gotoxy(6*e+c-2,4+d);  writeln('0:1'); gotoxy(4+(3*d)div(2),4*e+(2*c)div(3)); writeln('1:0'); a:=a+1; b:=b+1; i:=i+1; m[q+i]:=m[q+i]+1; end;
@@ -46,13 +52,15 @@ clrscr;
 		until a=(n-e);
 		d:=d+4;  q:=q+1; i:=0;
 	until b=(n*(n-1)/2);
- 	a:=0; q:=1;
-	gotoxy(4,5+d+n); writeln('hrac      body     poradi');
+	a:=0; q:=1; d:=d-n;
+	gotoxy(1,(n+1)*5); writeln('                                                            ');
+	gotoxy(1,(n+1)*5-1); writeln('                                ');
+	gotoxy(5,9+d+n); writeln('hrac      body     poradi');
 	for a:=1 to n do begin
-		gotoxy(4,6+d+n); writeln(a); d:=d+1;
+		gotoxy(6,10+d+n); writeln(a); d:=d+1;
 	end; d:=d-n; i:=0;
 	for q:=1 to n do begin
-		gotoxy(14,6+d+n); writeln(m[q]:0:1); d:=d+1;
+		gotoxy(15,10+d+n); writeln(m[q]:0:1); d:=d+1;
 	end; d:=d-n;
 	a:=0; b:=0; c:=0; e:=e+0;
 	repeat
@@ -68,8 +76,8 @@ clrscr;
 
 	repeat
 	q:=q+1;
-	if x[q]=z[q] then begin gotoxy(25,6+d+n); writeln(x[q]); d:=d+1; end;
-	if x[q]<>z[q] then begin gotoxy(25,6+d+n); writeln(x[q],' - ',z[q]); d:=d+1; end;
+	if x[q]=z[q] then begin gotoxy(26,10+d+n); writeln(x[q]); d:=d+1; end;
+	if x[q]<>z[q] then begin gotoxy(26,10+d+n); writeln(x[q],' - ',z[q]); d:=d+1; end;
 	until q=n;
 writeln(' ');
 writeln(' ');
